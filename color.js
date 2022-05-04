8c5aa689b75d017caffb1d3bc93910fe3afcf776
arnaud <arnaud.beaugeon@live.fr>
2022-05-04 14:35:53 +0200

    [+] Add catchColor function

--- color.js
+++ color.js
@@ -1,7 +1,8 @@
 for (var i = 0; i < 360; i++) {
   var color = document.createElement("span");
   color.setAttribute("id", "d" + i);
-	color.setAttribute('onclick', 'catchColor()');
+	color.setAttribute('onclick', `catchColor(${i})`);
+	color.setAttribute('class', 'cursor');
   color.style.backgroundColor = "hsl(" + i + ", 100%, 50%)";
   color.style.msTransform = "rotate(" + i + "deg)";
   color.style.webkitTransform = "rotate(" + i + "deg)";
@@ -11,8 +12,7 @@ for (var i = 0; i < 360; i++) {
   document.getElementById("colorwheel").appendChild(color);
 }
 
-function catchColor() {
-	let cursor = document.querySelector('span');
-	let a = cursor.getAttribute('style');
+function catchColor(e) {
+	let a = document.getElementById('d' + e);
 	console.log(a);
 }
