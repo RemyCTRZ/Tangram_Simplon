90faee0137e9399121e7c75dccdb1958cd126500
arnaud <arnaud.beaugeon@live.fr>
2022-05-04 16:29:55 +0200

    [~] Colorpicker make same color everywhere

--- color.js
+++ color.js
@@ -1,8 +1,8 @@
 for (var i = 0; i < 360; i++) {
   var color = document.createElement("span");
   color.setAttribute("id", "d" + i);
-	color.setAttribute('onclick', `catchColor(${i})`);
-	color.setAttribute('class', 'cursor');
+  color.setAttribute("onclick", `catchColor(${i})`);
+  color.setAttribute("class", "cursor");
   color.style.backgroundColor = "hsl(" + i + ", 100%, 50%)";
   color.style.msTransform = "rotate(" + i + "deg)";
   color.style.webkitTransform = "rotate(" + i + "deg)";
@@ -13,7 +13,17 @@ for (var i = 0; i < 360; i++) {
 }
 
 function catchColor(e) {
-	let a = document.getElementById('d' + e);
-	let picker = a.style.backgroundColor;
-	console.log(picker);
+  let a = document.getElementById("d" + e);
+  let forme = document.getElementsByClassName("forme");
+  let picker = a.style.backgroundColor;
+
+  for (let i = 0; i < 7; i++) {
+    if (i === 2) {
+      forme[2].setAttribute("style", `background-color: ${picker}`);
+    } else if (i === 4) {
+      forme[4].setAttribute("style", `background-color: ${picker}`);
+    } else {
+      forme[i].setAttribute("style", `border-color: ${picker}`);
+    }
+  }
 }
