da857524cec4175929a668bb03eee898a6a26288
RemyC <remycottrez@gmail.com>
2022-05-04 12:05:35 +0200

    Merge branch 'master' of https://github.com/RemyCTRZ/Tangram_Simplon

--- script.js
+++ script.js
@@ -26,8 +26,19 @@ function formeClick(e) {
     case 'camel':
       body.classList.add("camel");
       break;
-    case 'night_mode':
-      body.classList.add("night_mode");
-      break;
+  }
+}
+
+function nightMode() {
+  let body = document.body;
+  let icon = document.getElementById('icon_night_mode')
+  body.getAttribute("id") === "nightMode" ? body.removeAttribute("id") : body.setAttribute("id", "nightMode");
+  if (icon.className === "fa-solid fa-sun")
+  {
+    icon.className = "fa-solid fa-moon";
+  }
+  else
+  {
+    icon.className = "fa-solid fa-sun";
   }
 }
